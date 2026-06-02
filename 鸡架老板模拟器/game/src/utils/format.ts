4e5323c388a035e53a29566bbc42e85e3ede0e7e
yoosiwoo46 <yoosiwoo@163.com>
2026-06-02 22:52:29 +0800

    Delete 鸡架老板模拟器 directory

--- 鸡架老板模拟器/game/src/utils/format.ts
+++
@@ -1,6 +0,0 @@
-export function fmtMoney(n: number): string {
-  const fixed = n.toFixed(1)
-  const parts = fixed.split('.')
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  return parts.join('.')
-}
